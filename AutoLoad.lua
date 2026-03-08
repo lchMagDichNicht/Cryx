05e84fd6496b2716fa9a864da8a135f9655ad17a
HelloKittyWare <maxi73118@gmail.com>
2026-03-08 11:56:45 +0100

    Create AutoLoad.lua

---
+++ AutoLoad.lua
@@ -0,0 +1,16 @@
+local placeId = game.PlaceId
+
+-- Criminality
+if placeId == 8343259840 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lchMagDichNicht/Cryx/refs/heads/main/Criminality%20Beta.lua"))()
+
+-- 
+elseif placeId == 0 then
+
+
+-- 
+elseif placeId == 0 then
+
+else
+    print("No script available for this place id")
+end
